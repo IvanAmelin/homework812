f6cca732047ee1361a47e52f0070522c330e94b9
Ivan Amelin <ivmelinkvt@mail.ru>
2021-12-19 21:23:39 +0300

    Initial commit1

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,88 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int maxRadioNumber;
+    private int minRadioNumber;
+    private int currentRadioNumber;
+    private int maxVolume;
+    private int minVolume;
+    private int currentVolume;
+
+    public int getMaxRadioNumber() {
+        return maxRadioNumber;
+    }
+
+    public void setMaxRadioNumber(int maxRadioNumber) {
+        this.maxRadioNumber = maxRadioNumber;
+    }
+
+    public int getMinRadioNumber() {
+        return minRadioNumber;
+    }
+
+    public void setMinRadioNumber(int minRadioNumber) {
+        this.minRadioNumber = minRadioNumber;
+    }
+
+    public int getCurrentRadioNumber() {
+        return currentRadioNumber;
+    }
+
+    public void setCurrentRadioNumber(int currentRadioNumber) {
+        this.currentRadioNumber = currentRadioNumber;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+    public int increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        return currentVolume;
+    }
+    public int decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+        return currentVolume;
+    }
+    public int increaseRadioNumber() {
+        if (currentRadioNumber < 9) {
+            currentRadioNumber = currentRadioNumber + 1;
+        }
+        if (currentRadioNumber == 9 ) {
+            currentRadioNumber = 1;
+        }
+        return currentRadioNumber;
+    }
+    public int decreaseRadioNumber() {
+        if (currentRadioNumber > 1) {
+            currentRadioNumber = currentRadioNumber - 1;
+        }
+        if (currentRadioNumber == 1) {
+            currentRadioNumber = 9;
+        }
+        return currentRadioNumber;
+    }
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,100 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    @Test
+    public void shouldGetAndSetNumber() {
+        int expected = 5;
+        Radio radio = new Radio();
+        radio.setMaxRadioNumber(9);
+        radio.setMinRadioNumber(1);
+        radio.setCurrentRadioNumber(5);
+        assertEquals(expected, radio.getCurrentRadioNumber());
+    }
+
+    @Test
+    public void shouldGetAndSetVolume() {
+        int expected = 5;
+        Radio radio = new Radio();
+        radio.setMaxVolume(10);
+        radio.setMinVolume(0);
+        radio.setCurrentVolume(5);
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldUpVolume() {
+        int expected = 6;
+        Radio radio = new Radio();
+        radio.setMaxVolume(10);
+        radio.setMinVolume(0);
+        radio.setCurrentVolume(5);
+        radio.increaseVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    @Test
+    public void shouldDownVolume() {
+        int expected = 4;
+        Radio radio = new Radio();
+        radio.setMaxVolume(10);
+        radio.setMinVolume(0);
+        radio.setCurrentVolume(5);
+        radio.decreaseVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    @Test
+    public void shouldGoFrom10To10Volume() {
+        int expected = 10;
+        Radio radio = new Radio();
+        radio.setMaxVolume(10);
+        radio.setMinVolume(0);
+        radio.setCurrentVolume(10);
+        radio.increaseVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+    @Test
+    public void shouldGoFrom0To0Volume() {
+        int expected = 0;
+        Radio radio = new Radio();
+        radio.setMaxVolume(10);
+        radio.setMinVolume(0);
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldUpRadioNumber() {
+        int expected = 6;
+        Radio radio = new Radio();
+        radio.setMaxRadioNumber(9);
+        radio.setMinRadioNumber(1);
+        radio.setCurrentRadioNumber(5);
+        radio.increaseRadioNumber();
+        assertEquals(expected, radio.getCurrentRadioNumber());
+    }
+
+    @Test
+    public void shouldDownRadioNumber() {
+        int expected = 4;
+        Radio radio = new Radio();
+        radio.setMaxRadioNumber(9);
+        radio.setMinRadioNumber(1);
+        radio.setCurrentRadioNumber(5);
+        radio.decreaseRadioNumber();
+        assertEquals(expected, radio.getCurrentRadioNumber());
+    }
+    @Test
+    public void shouldGo9To1RadioNumber() {
+        int expected = 1;
+        Radio radio = new Radio();
+        radio.setMaxRadioNumber(9);
+        radio.setMinRadioNumber(1);
+        radio.setCurrentRadioNumber(9);
+        radio.increaseRadioNumber();
+        assertEquals(expected, radio.getCurrentRadioNumber());
+    }
+}
